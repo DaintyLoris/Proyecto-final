ebf94b087c30c0dd61fd75f20632a06408b00513
[Zero] <93053401+DaintyLoris@users.noreply.github.com>
2023-11-26 17:05:58 -0600

    Add files via upload

---
+++ K-nearest_neighbors.py
@@ -0,0 +1,33 @@
+from ucimlrepo import fetch_ucirepo
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+
+# fetch dataset
+zoo = fetch_ucirepo(id=111)
+
+X = zoo.data.features
+y = zoo.data.targets
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = KNeighborsClassifier()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred, average='weighted')
+recall = recall_score(y_test, y_pred, average='weighted')
+
+cm = confusion_matrix(y_test, y_pred)
+specificity = cm[0, 0] / (cm[0, 0] + cm[0, 1])
+
+f1 = f1_score(y_test, y_pred, average='weighted')
+
+print(f"Accuracy: {accuracy:.2f}")
+print(f"Precision: {precision:.2f}")
+print(f"Recall (Sensitivity): {recall:.2f}")
+print(f"Specificity: {specificity:.2f}")
+print(f"F1 Score: {f1:.2f}")
---
+++ naive_bayes.py
@@ -0,0 +1,31 @@
+from ucimlrepo import fetch_ucirepo
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+
+zoo = fetch_ucirepo(id=111)
+
+X = zoo.data.features
+y = zoo.data.targets
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = GaussianNB()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred, average='weighted')
+recall = recall_score(y_test, y_pred, average='weighted')
+
+cm = confusion_matrix(y_test, y_pred)
+specificity = cm[0, 0] / (cm[0, 0] + cm[0, 1])
+
+f1 = f1_score(y_test, y_pred, average='weighted')
+
+print(f"Accuracy: {accuracy:.2f}")
+print(f"Precision: {precision:.2f}")
+print(f"Recall (Sensitivity): {recall:.2f}")
+print(f"Specificity: {specificity:.2f}")
+print(f"F1 Score: {f1:.2f}")
